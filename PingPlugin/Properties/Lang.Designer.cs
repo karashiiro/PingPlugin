2271ea8ff4e6357f99eb1926db5d94fe1f7452d6
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-06-09 21:31:44 -0700

    chore: add translation rows for window titles

--- PingPlugin/Properties/Lang.Designer.cs
+++ PingPlugin/Properties/Lang.Designer.cs
@@ -70,6 +70,15 @@ namespace PingPlugin.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to PingPlugin Configuration.
+        /// </summary>
+        internal static string ConfigurationWindowTitle {
+            get {
+                return ResourceManager.GetString("ConfigurationWindowTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Defaults.
         /// </summary>
         internal static string Defaults {
@@ -183,6 +192,15 @@ namespace PingPlugin.Properties {
         internal static string MonitorOpacity {
             get {
                 return ResourceManager.GetString("MonitorOpacity", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Ping Graph.
+        /// </summary>
+        internal static string PingGraphTitle {
+            get {
+                return ResourceManager.GetString("PingGraphTitle", resourceCulture);
             }
         }
         
