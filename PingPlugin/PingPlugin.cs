3225be20fad356233338691d050c093254f8451c
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-08-09 20:28:55 -0700

    Copy FPSPlugin's ScrLk UI hiding hook

--- PingPlugin/PingPlugin.cs
+++ PingPlugin/PingPlugin.cs
@@ -5,9 +5,8 @@ using PingPlugin.Attributes;
 using PingPlugin.PingTrackers;
 using System;
 using System.Dynamic;
-using System.IO;
-using System.Reflection;
-using CheapLoc;
+using System.Runtime.InteropServices;
+using Dalamud.Hooking;
 
 namespace PingPlugin
 {
@@ -18,7 +17,11 @@ namespace PingPlugin
         private PingConfiguration config;
         
         private AggregatePingTracker pingTracker;
+        private bool uiHidden;
+        private Hook<ToggleUIDelegate> toggleUIHook;
         private PingUI ui;
+
+        private delegate IntPtr ToggleUIDelegate(IntPtr baseAddress, byte unknownByte);
 
         public string Name => "PingPlugin";
 
@@ -33,8 +36,8 @@ namespace PingPlugin
             // Set up ping trackers
             this.pingTracker = new AggregatePingTracker(this.config,
                 new ComponentModelPingTracker(this.config),
-                new Win32APIPingTracker(this.config),
-                new LinuxViaWinePingTracker(this.config));
+                new Win32APIPingTracker(this.config)
+                /*new LinuxViaWinePingTracker(this.config)*/);
             this.pingTracker.OnPingUpdated += (payload) =>
             {
                 dynamic obj = new ExpandoObject();
@@ -51,6 +54,16 @@ namespace PingPlugin
             this.pluginInterface.UiBuilder.OnOpenConfigUi += (sender, e) => this.ui.ConfigVisible = true;
             this.pluginInterface.UiBuilder.OnBuildUi += this.ui.BuildUi;
 
+            // Lifted from FPSPlugin, hook the ScrLk UI toggle; the client condition doesn't handle this
+            var toggleUiPtr = this.pluginInterface.TargetModuleScanner.ScanText("48 89 5C 24 ?? 48 89 74 24 ?? 57 48 83 EC 20 0F B6 B9 ?? ?? ?? ?? B8 ?? ?? ?? ??");
+            this.toggleUIHook = new Hook<ToggleUIDelegate>(toggleUiPtr, new ToggleUIDelegate((ptr, b) =>
+            {
+                PluginLog.Log("Hit!");
+                this.uiHidden = (Marshal.ReadByte(ptr, 104008) & 4) == 0;
+                return this.toggleUIHook.Original(ptr, b);
+            }));
+            this.toggleUIHook.Enable();
+
             // Initialize command manager
             this.commandManager = new PluginCommandManager<PingPlugin>(this, this.pluginInterface);
         }
@@ -59,7 +72,8 @@ namespace PingPlugin
         {
             this.ui.CutsceneActive = this.pluginInterface.ClientState.Condition[ConditionFlag.OccupiedInCutSceneEvent] ||
                                      this.pluginInterface.ClientState.Condition[ConditionFlag.WatchingCutscene] ||
-                                     this.pluginInterface.ClientState.Condition[ConditionFlag.WatchingCutscene78];
+                                     this.pluginInterface.ClientState.Condition[ConditionFlag.WatchingCutscene78] ||
+                                     this.uiHidden;
         }
 
         [Command("/ping")]
@@ -94,6 +108,8 @@ namespace PingPlugin
             if (disposing)
             {
                 this.commandManager.Dispose();
+
+                this.toggleUIHook?.Disable();
 
                 this.pluginInterface.UiBuilder.OnOpenConfigUi -= (sender, e) => this.ui.ConfigVisible = true;
                 this.pluginInterface.UiBuilder.OnBuildUi -= this.ui.BuildUi;
