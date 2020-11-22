67e7b7cbf855afb1512c525a8d4affaac7d1c719
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-11-21 17:09:23 -0800

    Prevent saved ping count from being less than 1

--- PingPlugin/PingUI.cs
+++ PingPlugin/PingUI.cs
@@ -103,7 +103,7 @@ namespace PingPlugin
             var queueSize = this.config.PingQueueSize;
             if (ImGui.InputInt(Loc.Localize("RecordedPings", string.Empty), ref queueSize))
             {
-                this.config.PingQueueSize = queueSize;
+                this.config.PingQueueSize = Math.Max(queueSize, 1);
                 this.config.Save();
             }
 
