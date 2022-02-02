4a5c1a3b30dce13d3f741855dfc0c61c7d5a6470
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-02-02 09:28:54 -0800

    Version bump

--- PingPlugin/PingTrackers/IpHlpApiPingTracker.cs
+++ PingPlugin/PingTrackers/IpHlpApiPingTracker.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Linq;
+﻿using Dalamud.Game.ClientState;
+using System;
 using System.Net;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
-using Dalamud.Game.ClientState;
-using Dalamud.Logging;
 
 namespace PingPlugin.PingTrackers
 {
