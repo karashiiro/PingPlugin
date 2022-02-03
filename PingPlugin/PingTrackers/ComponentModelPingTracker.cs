bfe52e7a29470ed45490c08ce61d7db878275ae4
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-02-02 19:39:35 -0800

    Log warning when COM ping fails

--- PingPlugin/PingTrackers/ComponentModelPingTracker.cs
+++ PingPlugin/PingTrackers/ComponentModelPingTracker.cs
@@ -32,6 +32,10 @@ namespace PingPlugin.PingTrackers
                         {
                             NextRTTCalculation((ulong)pingReply.RoundtripTime);
                         }
+                        else
+                        {
+                            PluginLog.LogWarning($"Got bad status {pingReply.Status} when executing ping - this may be temporary and acceptable.");
+                        }
                     }
                     catch (Exception e)
                     {
