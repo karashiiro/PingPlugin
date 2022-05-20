bff0de87968a2d6158ebb20ec973cce33565ad0b
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-05-19 21:00:48 -0700

    Add ping tracker enum

---
+++ PingPlugin/PingTrackers/PingTrackerKind.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PingPlugin.PingTrackers
+{
+    public enum PingTrackerKind
+    {
+        COM,
+        IpHlpApi,
+        Packets,
+    }
+
+    public static class PingTrackerKindExtensions
+    {
+        public static string FormatName(this PingTrackerKind kind)
+        {
+            return kind switch
+            {
+                PingTrackerKind.COM => "COM",
+                PingTrackerKind.IpHlpApi => "Win32 API",
+                PingTrackerKind.Packets => "Game packets",
+                _ => throw new ArgumentOutOfRangeException(nameof(kind)),
+            };
+        }
+    }
+}
